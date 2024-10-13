dd723703d60b26f639fc89cd50b6e0e09c5796e7
Minseop <47380148+Neogr1@users.noreply.github.com>
2024-10-13 21:28:04 +0900

    Title: 동전 합의 경우의 수 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241013/동전 합의 경우의 수 2/number-of-coin-agreement-cases-2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n,k = map(int, input().split())
+values = [int(input()) for _ in range(n)]
+
+counts = [k] * (k+1)
+counts[0] = 0
+for i in range(1, k+1):
+    for v in values:
+        if i-v >= 0:
+            counts[i] = min(counts[i], counts[i-v]+1)
+
+print(counts[k])
